d7f036d8570a015de1ed3b70cf8231387880d067
AlexWalker28 <alexwolk28@gmail.com>
2017-04-14 19:54:44 +0600

    Send sms

--- app/src/main/java/com/example/alexwalker/sendsmsapp/MainActivity.java
+++ app/src/main/java/com/example/alexwalker/sendsmsapp/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.alexwalker.sendsmsapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText phoneNumber;
-    EditText message;
+    EditText phoneNumberEditText;
+    EditText messageEditText;
     Button sendButton;
+    String phoneNumber;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +27,36 @@ public class MainActivity extends AppCompatActivity {
 
         init();
 
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phoneNumber = phoneNumberEditText.getText().toString();
+                message = messageEditText.getText().toString();
+                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+                    try{
+                        sendMessage(phoneNumber, message);
+                        Toast.makeText(getApplicationContext(), "Sms sent", Toast.LENGTH_LONG).show();
+                    }catch (Exception e){
+                        Toast.makeText(getApplicationContext(), "Sms fail. Please try again", Toast.LENGTH_LONG).show();
+                        Log.v("SMS", "sms failed: " + e);
+                        e.printStackTrace();
+                    }
+                } else ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.SEND_SMS},1);
+
+
+            }
+        });
+
+    }
+
+    private void sendMessage(String phoneNumber, String message) {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(phoneNumber, null, message, null, null);
     }
 
     private void init() {
-        phoneNumber = (EditText)findViewById(R.id.phoneNumberEditText);
-        message = (EditText)findViewById(R.id.messageEditText);
+        phoneNumberEditText = (EditText)findViewById(R.id.phoneNumberEditText);
+        messageEditText = (EditText)findViewById(R.id.messageEditText);
         sendButton = (Button)findViewById(R.id.sendButton);
     }
 }
