31fe3ec6acca3f072674d26750ef5b5b708bc0f8
erlanamanatov <erlanamanatov@mail.ru>
2017-05-06 01:08:49 +0600

    cloud function updated: new function groupMemberListener 1) if member added ==> adds groupName  to user's groups 2) if member deleted ==> removes groupName from user's groups

--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,13 @@ exports.checkRequestApprovements = functions.database.ref('/Groups/{group}/reque
     .onWrite(event => {
         const request = event.data.val();
         const requestKey = event.data.key;
-        approvementsCount = request.agreeCount;
+        var approvementsCount = request.agreeCount;
         if (approvementsCount == 2) {
             var ref = event.data.ref.parent.parent;
             var groupName = ""
             return ref.once("value")
                 .then(function(snapshot){
                     groupName = snapshot.child("name").val();
-                }).then(() => {
-                    event.data.ref.parent.parent.parent.parent.child('Users').child(request.userId).child('groups').child(groupName).set(true);
                 }).then(() => {
                     event.data.ref.parent.parent.parent.parent.child('Users').child(request.userId).child('pending').child(groupName).set(null);
                 }).then(() => {
@@ -24,4 +22,23 @@ exports.checkRequestApprovements = functions.database.ref('/Groups/{group}/reque
                     event.data.ref.parent.child(requestKey).set(null);
                 });
       }
+    });
+
+exports.groupMemberListener = functions.database.ref('/Groups/{group}/members/{member}')
+    .onWrite( event => {
+        const userId = event.data.key;
+        const groupName = event.data.ref.parent.parent.key;
+        console.log(userId, groupName);
+        const userGroupMembershipRef = admin.database().ref('Users').child(userId).child('groups').child(groupName);
+
+        if (event.data.exists() && !event.data.previous.exists()) {
+            console.log("On member added");
+            return userGroupMembershipRef.set(true);
+        }
+        else if (!event.data.exists() && event.data.previous.exists()) {
+            console.log("On member deleted");
+            return userGroupMembershipRef.set(null);
+        } else {
+            return;
+        }
     });
