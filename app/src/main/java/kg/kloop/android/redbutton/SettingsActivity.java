b88286730ccc7a186104a02575226ffd8d48e3f8
NismoSkyline <nismoskyr34@gmail.com>
2017-05-07 20:43:15 +0600

    New Design

--- app/src/main/java/kg/kloop/android/redbutton/SettingsActivity.java
+++ app/src/main/java/kg/kloop/android/redbutton/SettingsActivity.java
@@ -41,6 +41,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        //getSupportActionBar().hide();
 
         init();
         loadDataFromPref();
